74bd68225f1e758e1a930f5c9d16e897a8ab7d9f
Kienpv <omomatic246@gmail.com>
2018-02-02 16:32:00 +0700

    chap5 - pull|1.0

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   scope "(:locale)", locale: /en|vi/ do
-    get "/help" => "static_pages#help", as: :help
-    get "/about" => "static_pages#about", as: :about
-    get "/contact" => "static_pages#contact", as: :contact
+    get "/help", to: "static_pages#help", as: :help
+    get "/about", to: "static_pages#about", as: :about
+    get "/contact", to: "static_pages#contact", as: :contact
+    get "/signup", to: "users#new", as: :signup
     root "static_pages#home"
   end
 end
